171fc53b907f2d3d5826b258d1ae591039c23dfb
foxmon <jjockrod@naver.com>
2022-08-02 21:26:00 +0900

    prototype

--- prototype/prototype01.js
+++ prototype/prototype01.js
@@ -1,27 +1,43 @@
 class Person {
-  constructor(name) {
-    this.name = name;
-  }
+    constructor(name) {
+        this.name = name
+    }
 
-  sayHi() { 
-    console.log(`Hi! My name is ${this.name}`); 
-  }
-  
-  static sayHello() { 
-    console.log('Hello!'); 
-  }
+    sayHi() {
+        console.log(`Hi! My name is ${this.name}`)
+    }
+
+    static sayHello() {
+        console.log("Hello!")
+    }
 }
 
-Person.prototype.hi = function() {
-  console.log(`Hi! My name is ${this.name}`);
+Person.prototype.hi = function () {
+    console.log(`Hi! My name is ${this.name}`)
 }
 
 // class는 hoisting 된다.
-const me = new Person('Lee');
-me.hi2 = function() {
-  console.log("Hello Hello!");
+const me = new Person("Lee")
+me.hi2 = function () {
+    console.log("Hello Hello!")
 }
-me.sayHi();
-Person.sayHello();
-me.hi();
-me.hi2();
+me.sayHi()
+Person.sayHello()
+me.hi()
+me.hi2()
+
+function A() {
+    this.name = "A"
+}
+A.prototype.getA = function () {
+    return this.name
+}
+const a = new A()
+console.log(a.getA())
+
+// 밑에는 그냥 내부함수 느낌인 것 같은데..
+A.getA = function () {
+    return "B"
+}
+const b = new A()
+console.log(b.getA())
