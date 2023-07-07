5b87d241b824c55a4a8b86673c0beb86901200b1
foxmon <jjockrod@naver.com>
2023-07-07 11:07:27 +0900

    typeof

--- Type/typeof1.js
+++ Type/typeof1.js
@@ -1,7 +1,11 @@
-let a = 3
+let a = 3;
 b = "3";
 c = true;
+
+function d() {}
 
 console.log(typeof a);
 console.log(typeof b);
 console.log(typeof c);
+console.log(typeof d);
+console.log(typeof {});
